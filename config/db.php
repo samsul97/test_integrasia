97707df63aeb28127a7ff58aa8c45353133a369f
samsul97 <samsulaculhadi@gmail.com>
2020-01-30 06:21:52 -0800

    Test Integrasia Start

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'connectionString' => 'pgsql:host=localhost;port=5432;dbname=testintegrasia',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
